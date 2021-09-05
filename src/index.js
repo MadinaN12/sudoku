52cd30f26a1f4ef597fcba0275c7d4006d0b87c1
Madina <nishanbaeva.m.s.95@gmail.com>
2021-09-05 17:13:07 +0500

    init: sudoku

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  for (var row = 0; row < 9; row++) {
+    for (var col = 0; col < 9; col++) {
+      if (matrix[row][col] == 0) {       
+        for (var numb = 1; numb < 10; numb++) {
+          if (validNumb(matrix, row, col, numb)) {
+            matrix[row][col] = numb;
+            if (solveSudoku(matrix)) {
+              return matrix;
+            } else {
+              matrix[row][col] = 0;
+            }
+          }
+        }
+        return false;
+      }
+    }
+  }
+  return matrix;
+}
+
+function section(row, col, x) {
+  return {
+    r: Math.floor(row / 3) * 3 + Math.floor(x / 3),
+    c: Math.floor(col / 3) * 3 + x % 3
+  };
+}
+
+function validNumb(matrix, row, col, numb) {
+  for (var x = 0; x < 9; x++) {  
+    var pos = section(row, col, x);  
+    if (matrix[row][x] == numb || matrix[x][col] == numb || matrix[pos.r][pos.c] == numb) {
+      return false;
+    }
+  }
+  return true;
 }
